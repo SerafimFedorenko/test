06c2d4fce09182075e62695da0ba471c5677e8ad
SerafimFedorenko <92511061+SerafimFedorenko@users.noreply.github.com>
2021-10-30 16:02:20 +0300

    Deleting unnecessary usings

--- Chess/ChessLibraryTests/GameControllerTests.cs
+++ Chess/ChessLibraryTests/GameControllerTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ChessLibrary;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChessLibrary.Tests
 {
